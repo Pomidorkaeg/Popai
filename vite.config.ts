c0af657234d87c8c24c28b9b8768fe0e20a675b4
Егор <egornazarko2009.09@gmail.com>
2025-03-27 17:26:18 +0300

    Упрощена структура приложения для исправления проблемы с загрузкой

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Losal/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -13,11 +13,6 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    sourcemap: true
   }
 });
